cff7cf55dd94bd2d289f2c8efbf458e9d1b920cb
vika <v.sapunkova@neurobotics.ru>
2020-04-04 15:03:28 +0300

    commit

--- src/test/java/ru/netology/manager/ManagerTestNonEmptyWithSetup.java
+++ src/test/java/ru/netology/manager/ManagerTestNonEmptyWithSetup.java
@@ -10,7 +10,11 @@ public class ManagerTestNonEmptyWithSetup {
     private Poster third = new Poster(3, 3, "third", "action");
     private Poster fourth = new Poster(4, 4, "first", "comedy");
     private Poster fifth = new Poster(5, 5, "second", "drama");
-
+    private Poster sixth = new Poster(6, 6, "third", "action");
+    private Poster seventh = new Poster(7, 7, "first", "comedy");
+    private Poster eighth = new Poster(8, 8, "second", "drama");
+    private Poster ninth = new Poster(9, 9, "third", "action");
+    private Poster tenth = new Poster(10, 10, "first", "comedy");
 
     @BeforeEach
     public void setUp() {
@@ -19,14 +23,18 @@ public class ManagerTestNonEmptyWithSetup {
         manager.add(third);
         manager.add(fourth);
         manager.add(fifth);
-
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
     }
     @Test
     public void shouldRemoveIfExists() {
         int idToRemove = 1;
         manager.removeById(idToRemove);
         Poster[] actual = manager.getAll();
-        Poster[] expected = new Poster[]{fifth, fourth, third, second};
+        Poster[] expected = new Poster[]{tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second};
 //    assertEquals(expected, actual);
         assertArrayEquals(expected, actual);
     }
@@ -35,7 +43,7 @@ public class ManagerTestNonEmptyWithSetup {
         int idToRemove = 4;
         manager.removeById(idToRemove);
         Poster[] actual = manager.getAll();
-        Poster[] expected = new Poster[]{fifth, third, second, first};
+        Poster[] expected = new Poster[]{tenth, ninth, eighth, seventh, sixth, fifth, third, second, first};
         assertArrayEquals(expected, actual);
     }
 }
